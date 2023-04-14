c686eef6fb349fd6dfd72cf1c0f102032d9acb5d
воробйов александр <avorobeov21@gmail.com>
2023-04-14 20:45:37 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string STOP_COMMAND = "Exit";
+            bool isExit = false;
+            string imputText;
+
+            Console.WriteLine($"Для остановки ведите команду : {STOP_COMMAND}");
+
+            while (isExit == false)
+            {
+                Console.Write("Укажите текст: ");
+                imputText = Console.ReadLine();
+
+                if (imputText == STOP_COMMAND)
+                {
+                    isExit = true;
+                }
+
+                Console.Clear();
+                Console.WriteLine($"Вы вели: {imputText}");
+            }
+        }
+    }
+}
